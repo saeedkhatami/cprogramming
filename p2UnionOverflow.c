8e496362fae4ed5ef7385300f336e7b8d3f461c1
Saeed Khatami <0xb4dc0d3x@gmail.com>
2023-11-19 18:52:15 +0330

    p2UnionOverflow.c

---
+++ p2UnionOverflow.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct{
+	uint16_t pid;
+	uint8_t ptype;
+	uint32_t cputime;
+} process_info_struct;
+
+typedef union{
+	uint16_t pid;
+	uint8_t ptype;
+	uint32_t cputime;
+} process_info_union;
+
+int main(int argc, char *argv[]) {
+	process_info_struct p1;
+	process_info_union p2;
+	
+	p1.cputime = 4000000000;
+	p1.ptype = 'A';
+	
+	p2.cputime = 4000000000;
+	p2.ptype = 'A';
+	
+	printf("p1.cputime:           %lu \n", p1.cputime);
+	printf("p1.ptype:             %c \n", p1.ptype);
+	printf("--------------------------------\n");
+	printf("p2.cputime:           %lu \n", p2.cputime);
+	printf("p2.ptype:             %c \n", p2.ptype);
+	printf("--------------------------------\n");
+	printf("- here is cause overflow in p2 -\n");
+	p2.cputime = 4294967295;
+	p2.ptype = 'A';
+	printf("p2.cputime:           %lu \n", p2.cputime);
+	printf("%lu is invalid for 4 byte \n", p2.cputime);
+	
+	return 0;
+}
